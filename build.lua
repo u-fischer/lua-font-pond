cbf2f52af369bc94554bf1a1c47806b50dbb4ff2
Ulrike Fischer <fischer@troubleshooting-tex.de>
2018-05-11 21:29:41 +0200

    initial commit

---
+++ build.lua
@@ -0,0 +1,13 @@
+-- l3build check settings for lua-font-pond
+module="lua-font-pond"
+
+stdengine    = "luatex"
+checkengines = {"luatex"}
+checkconfigs = {"build","config-plain"}
+
+checkruns = 3
+
+kpse.set_program_name ("kpsewhich")
+if not release_date then
+ dofile ( kpse.lookup ("l3build.lua"))
+end
