fdee00aefaadb61a79a136c04e70d4a8605656cb
Ulrike Fischer <fischer@troubleshooting-tex.de>
2018-07-27 18:54:03 +0200

    testing unicode-math tests on travis

--- build.lua
+++ build.lua
@@ -3,7 +3,7 @@ module="lua-font-pond"
 
 stdengine    = "luatex"
 checkengines = {"luatex"}
-checkconfigs = {"build","config-plain","config-fontspec"}
+checkconfigs = {"build","config-plain","config-fontspec","config-unicode-math"}
 
 testsuppdir =  "./resources" -- test files should also use resources
 
